893643b357c14c0c1fc5d7e18fc14f8400f33843
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2023-03-20 21:33:17 +0300

    Сделал авторизацию, верстку(  нужно еще доделать)

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="wrapper">
-      <Header />
+      {/* <Header /> */}
       <div className="content">
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Login />}></Route>
           </Routes>
         </div>
       </div>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
-
+import logo from '../assets/img/ball.svg';
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="container">
+      <div className="auth">
+        <img className="auth__logo" width="44" src={logo} alt="Valleyball logo"></img>
+        <h2 className="auth__title">Авторизация</h2>
+        <div className="auth__inputs">
+          <input
+            className="auth__input auth__input_login input"
+            placeholder="Логин или телефон"></input>
+          <input className="auth__input auth__input_password input" placeholder="Пароль"></input>
+        </div>
+        <p className="auth__text">
+          Нет аккаунта? <a className="auth__link">Зарегистрироваться</a>
+        </p>
+        <button className="auth__button">Войти</button>
+      </div>
+    </div>
+  );
 };
 export default Login;

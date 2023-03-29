badefff94d1d89212ab97982dbe111fb1fc81625
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-03-29 23:07:27 +0300

    inputs made with map

--- src/pages/Registr.tsx
+++ src/pages/Registr.tsx
@@ -3,18 +3,46 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/img/ball.svg';
 import AuthInputs from '../components/AuthInputs';
 
+type Inputs = {
+  placeholder: string;
+  type: string;
+  onChange: (value: string) => void;
+}[];
+
 export const Registr = () => {
+  const [fio, setFio] = React.useState<string>('');
+  const [tel, setTel] = React.useState<string>('');
+  const [group, setGroup] = React.useState<string>('');
+  const [login, setLogin] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+  const [passwordCheck, setPasswordCheck] = React.useState<string>('');
+
+  const inputs: Inputs = [
+    { placeholder: 'ФИО', type: 'text', onChange: (value) => setFio(value) },
+    { placeholder: 'Телефон', type: 'tel', onChange: (value) => setTel(value) },
+    { placeholder: 'Группа', type: 'text', onChange: (value) => setGroup(value) },
+    { placeholder: 'Логин', type: 'text', onChange: (value) => setLogin(value) },
+    { placeholder: 'Пароль', type: 'password', onChange: (value) => setPassword(value) },
+    {
+      placeholder: 'Повторите пароль',
+      type: 'password',
+      onChange: (value) => setPasswordCheck(value),
+    },
+  ];
+
   return (
     <div className='auth'>
       <img className='auth__logo' width='44' src={logo} alt='Valleyball logo'></img>
       <h2 className='auth__title'>Регистрация</h2>
       <div className='auth__inputs'>
-        <AuthInputs placeholder='ФИО' type='text' />
-        <AuthInputs placeholder='Телефон' type='tel' />
-        <AuthInputs placeholder='Группа' type='text' />
-        <AuthInputs placeholder='Логин' type='text' />
-        <AuthInputs placeholder='Пароль' type='password' />
-        <AuthInputs placeholder='Повторите пароль' type='password' />
+        {inputs.map((obj, i) => (
+          <AuthInputs
+            key={i}
+            placeholder={obj.placeholder}
+            type={obj.type}
+            onChangeInput={obj.onChange}
+          />
+        ))}
       </div>
       <p className='auth__text'>
         Уже есть аккаунт?&nbsp;

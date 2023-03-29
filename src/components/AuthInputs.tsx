e8a7ea9ac8c20d2f2f14e56bcb3f05712c9eef7a
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-03-29 23:07:11 +0300

    inputs made controllable

--- src/components/AuthInputs.tsx
+++ src/components/AuthInputs.tsx
@@ -3,12 +3,25 @@ import React from 'react';
 type AuthInputsProps = {
   placeholder: string;
   type: string;
+  onChangeInput: (value: string) => void;
 };
 
-export const AuthInputs: React.FC<AuthInputsProps> = ({ placeholder, type }) => {
+export const AuthInputs: React.FC<AuthInputsProps> = ({ placeholder, type, onChangeInput }) => {
+  const [value, setValue] = React.useState<string>('');
+
+  const onChangeInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    onChangeInput(event.target.value);
+  };
+
   return (
     <div className='auth__for-input'>
-      <input className='auth__input input' placeholder={placeholder} type={type}></input>
+      <input
+        value={value}
+        onChange={onChangeInputValue}
+        className='auth__input input'
+        placeholder={placeholder}
+        type={type}></input>
     </div>
   );
 };

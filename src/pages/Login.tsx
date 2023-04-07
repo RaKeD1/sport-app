e28cb9692398ce587cd966c52b48af480a957ca0
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-04-07 20:28:42 +0300

    Update Login.tsx
    delete imports in login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 import React from 'react';
+=======
+import LoginForm from '../components/LoginForm';
+>>>>>>> Stashed changes
 
 export const Login = () => {
   return <div>Login</div>;

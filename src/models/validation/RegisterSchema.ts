1d9c2ae215487265a2aa3a702d5034672e7b86ff
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2023-04-08 07:28:34 +0300

    мини фикс верстки и add валидацию для ФИО

--- src/models/validation/RegisterSchema.ts
+++ src/models/validation/RegisterSchema.ts
@@ -8,9 +8,22 @@ const RegisterSchema = Yup.object().shape({
     //Если нет, то выводится сообщение в скобках
     .email('Некорректный email')
     .max(100, 'Максимальная длина 100 символов')
+
     //не сабмитим, если поле не заполнено
     .required('Обязательное поле'),
-  fio: Yup.string().required('Обязательное поле'),
+  fio: Yup.string()
+    .required('Обязательное поле')
+    .test('fio-match', 'Проверьте правильность введенных данных', function (value: string) {
+      if (
+        value.replace(/[^a-zа-яё]/gi, '').replace(/[а-я]/g, '').length === 3 &&
+        value.replace(/[a-zA-Zа-яА-Я]/g, '').length === 2
+      ) {
+        return true;
+        //Убираем все символы кроме русских букв
+      } else {
+        return false;
+      }
+    }),
   phone: Yup.string()
     .matches(phoneRegExp, 'Телефон должен содержать 10 цифр')
     .required('Обязательное поле'),

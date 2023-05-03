153e8245e671e04c9fd56ddb7fcba01d1262bec7
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-05-03 21:24:56 +0300

    add team validation and make always redirect

--- src/components/CreateTrain/index.tsx
+++ src/components/CreateTrain/index.tsx
@@ -1,23 +1,40 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import styles from './CreateTrain.module.scss';
 import { useAppDispatch } from '../../redux/store';
 import { useSelector } from 'react-redux';
 import { SelectCreateTrain, setPlayers, setTeam } from '../../redux/slices/createTrainSlice';
 import { SelectAccountID, Status } from '../../redux/slices/profileSlice';
-import { SelectTrainStatus, postNewTrain } from '../../redux/slices/trainSlice';
+import {
+  SelectTrainPlayers,
+  SelectTrainStatus,
+  clearTrain,
+  postNewTrain,
+  setLoading,
+} from '../../redux/slices/trainSlice';
 import { useNavigate } from 'react-router';
 import classNames from 'classnames';
 import AsyncSearchBar from '../AsyncSearchBar';
 import { ISelectUser } from '../../models/ISelectUser';
+import TrainService from '../../services/TrainService';
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 
 const CreateTrain: FC = () => {
   const [isValid, setIsValid] = useState<boolean>(false);
-  const { players, team } = useSelector(SelectCreateTrain);
+  const [teamIsValid, setTeamIsValid] = useState<boolean>(false);
+  const [teamValue, setTeamValue] = useState<string>('');
+  const { selectPlayers, team } = useSelector(SelectCreateTrain);
+  const players = useSelector(SelectTrainPlayers);
   const account_id = useSelector(SelectAccountID);
   const status = useSelector(SelectTrainStatus);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const [collabs, setCollabs] = useState<ISelectUser[]>([]);
+
+  // useEffect(() => {
+  //   console.log('STATUS', status);
+  //   console.log('players', players);
+  //   dispatch(clearTrain());
+  // }, []);
 
   useEffect(() => {
     console.log('collabs', collabs);
@@ -26,19 +43,33 @@ const CreateTrain: FC = () => {
   }, [collabs]);
 
   useEffect(() => {
-    if (!team || collabs.length === 0) setIsValid(false);
+    if (!teamIsValid || collabs.length === 0) setIsValid(false);
     else setIsValid(true);
-  }, [collabs, team]);
+  }, [collabs, teamIsValid]);
 
-  useEffect(() => {
-    if (status === Status.SUCCESS) {
-      navigate('/statistics');
-    }
-  }, [status]);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     if (status === Status.SUCCESS && players[0]) {
+  //       navigate('/statistics');
+  //     }
+  //   }, 1000);
+  // }, [status]);
 
-  const createTrain = (account_id: number, team: string, players: number[]) => {
-    console.log('Button clicked', account_id, team, players);
-    dispatch(postNewTrain({ account_id, team, players }));
+  const onChangeTeamInput = async (value: string) => {
+    if (value) {
+      const valid = await TrainService.checkTeam(value);
+      console.log('valid', valid.data);
+      if (valid.data) setTeamIsValid(true);
+      else setTeamIsValid(false);
+    } else setTeamIsValid(false);
+    setTeamValue(value);
+    dispatch(setTeam(value));
+  };
+
+  const createTrain = (account_id: number, team: string, selectPlayers: number[]) => {
+    console.log('Button clicked', account_id, team, selectPlayers);
+    dispatch(postNewTrain({ account_id, team, selectPlayers }));
+    navigate('/statistics');
   };
 
   return (
@@ -46,11 +77,25 @@ const CreateTrain: FC = () => {
       <h2 className={styles.train__title}>Создание тренировки</h2>
       <div className={styles.train__elem}>
         <label htmlFor=''>Название команды:</label>
-        <input
-          value={team}
-          onChange={(event) => dispatch(setTeam(event.target.value))}
-          type='text'
-        />
+        <div className={styles.train__team}>
+          <input
+            className={classNames({ [styles.train__team_notValid]: !teamIsValid && teamValue })}
+            value={teamValue}
+            onChange={(event) => onChangeTeamInput(event.target.value)}
+            type='text'
+          />
+          {!teamIsValid && teamValue && <span>У этой группы сегодня уже есть тренировка</span>}
+          {!teamIsValid && teamValue && (
+            <FaTimesCircle
+              className={classNames(styles.train__checkIcon, styles.train__checkIcon_false)}
+            />
+          )}
+          {teamIsValid && teamValue && (
+            <FaCheckCircle
+              className={classNames(styles.train__checkIcon, styles.train__checkIcon_true)}
+            />
+          )}
+        </div>
       </div>
       <div className={styles.train__elem}>
         <label htmlFor=''>Игроки:</label>
@@ -59,7 +104,7 @@ const CreateTrain: FC = () => {
       <button
         className={classNames(styles.train__create, { [styles.train__create_notValid]: !isValid })}
         disabled={!isValid}
-        onClick={() => createTrain(account_id, team, players)}>
+        onClick={() => createTrain(account_id, team, selectPlayers)}>
         Создать
       </button>
     </div>

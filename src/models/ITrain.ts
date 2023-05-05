f2c8b694d1c0b198fca3a736c0a6dfd82dabec4b
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-05-05 21:16:06 +0300

    add fio in ITrain

--- src/models/ITrain.ts
+++ src/models/ITrain.ts
@@ -1,11 +1,10 @@
 export interface ITrain {
-  name: string;
-  surname: string;
-  patronimyc: string;
+  fio: string;
   inning_stat: string;
   blocks_stat: string;
   attacks_stat: string;
   catch_stat: string;
   defence_stat: string;
   support_stat: string;
+  id_train: number;
 }

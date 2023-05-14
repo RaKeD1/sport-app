efa72b98b7de270602b0fbed6252c063f2cfa187
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-05-14 15:47:18 +0300

    fix select bar for conditions

--- src/components/SelectBar.tsx
+++ src/components/SelectBar.tsx
@@ -1,27 +1,24 @@
 import { FC, useEffect, useState } from 'react';
 import Select from 'react-select';
-
-type Option = {
-  value: string;
-  label: string;
-};
+import { Option } from './ActionModal';
 
 interface SelectBarProps {
-  data: string;
-  setSelectedCondition: (value: string) => void;
+  data: string[];
+  selectedCondition: Option;
+  setSelectedCondition: (selected: Option) => void;
 }
 
-const SelectBar: FC<SelectBarProps> = ({ data, setSelectedCondition }) => {
-  const [options, setOptions] = useState(
-    data.split('; ').map((option) => ({ value: option, label: option })),
-  );
+const SelectBar: FC<SelectBarProps> = ({ data, selectedCondition, setSelectedCondition }) => {
+  const [options, setOptions] = useState(data.map((option) => ({ value: option, label: option })));
+  const [selected, setSelected] = useState<Option>(null);
 
   useEffect(() => {
-    setOptions(data.split('; ').map((option) => ({ value: option, label: option })));
+    setOptions(data.map((option) => ({ value: option, label: option })));
     console.log(options);
   }, [data]);
 
-  const handleChange = (selectedOption: string) => {
+  const handleChange = (selectedOption: Option) => {
+    setSelected(selectedOption);
     setSelectedCondition(selectedOption);
   };
 
@@ -30,13 +27,15 @@ const SelectBar: FC<SelectBarProps> = ({ data, setSelectedCondition }) => {
       <Select
         className='basic-single'
         classNamePrefix='select'
+        key={`my_unique_select_key__${selectedCondition}`}
+        value={selectedCondition}
         placeholder='Выберите условие'
         noOptionsMessage={() => 'Нет условий'}
         getOptionLabel={(e: Option) => e.label}
         getOptionValue={(e: Option) => e.value}
         name='condition'
         options={options}
-        onChange={(e) => handleChange(e.value)}
+        onChange={(e) => handleChange(e)}
       />
     </>
   );

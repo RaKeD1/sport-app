42ef4fa1dd9d9b96c78200f2d046e4fda139d498
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-05-19 23:10:26 +0300

    IAction interface

---
+++ src/models/IAction.ts
@@ -0,0 +1,12 @@
+export interface IAction {
+  id_action: number;
+  id_action_type: number;
+  name_action: string;
+  result: string;
+  condition: string;
+  time: string;
+  day_team: string;
+  date: string;
+  id_train: number;
+  fio: string;
+}

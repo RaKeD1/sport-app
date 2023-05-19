98e05944eed826335afbee4767c7804f39354f3a
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-05-19 20:16:51 +0300

    fix stats showing in accordion

--- src/components/Accordion/Placeholder.tsx
+++ src/components/Accordion/Placeholder.tsx
@@ -27,12 +27,18 @@ export const ContentPlaceholder = ({ data, onClickAddAction }) => (
     className='content-placeholder'>
     {Object.entries(data)
       .filter((arr) => arr[0] !== 'fio' && arr[0] !== 'id_train')
-      .map((arr, index) => (
-        <div key={index} className='accordion__content_list'>
-          <div className='accordion__content_list_text'>{columnNames[arr[0]]}</div>
-          <div className='accordion__content_rez'>{arr[1].toString()}</div>
-        </div>
-      ))}
+      .map((arr, index) => {
+        const num = arr[1].toString().replace('0.', '');
+
+        return (
+          <div key={index} className='accordion__content_list'>
+            <div className='accordion__content_list_text'>{columnNames[arr[0]]}</div>
+            <div className='accordion__content_rez'>
+              {num + (num.length === 1 && num !== '0' ? '0%' : '%')}
+            </div>
+          </div>
+        );
+      })}
     <button className='accordion__button' onClick={() => onClickAddAction(data.id_train)}>
       Добавить
     </button>

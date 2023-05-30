4021a63b26d9a0d972d8503c71f4ab0fc743487d
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2023-05-30 11:15:03 +0300

    delete trash

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,45 +3,21 @@ import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import logo from '../assets/img/ball.svg';
 import { FaUserAlt } from 'react-icons/fa';
+import Hamburger from './Humburger';
 
 const Header: React.FC = () => {
-  const location = useLocation();
-  const pages = [
-    {
-      path: '/createtraining',
-      label: 'Создать тренировку',
-    },
-    {
-      path: '/training',
-      label: 'Редактировать тренировку',
-    },
-    {
-      path: '/statistics',
-      label: 'Посмотреть статистику',
-    },
-    {
-      path: '/players',
-      label: 'Игроки',
-    },
-    {
-      path: '/',
-      label: 'Профиль',
-      fa: <FaUserAlt className='profileIcon' />,
-    },
-  ];
   return (
     <div className='header'>
       <nav className='container'>
-        <ul className='header__nav'>
-          <div className='header__logo'>
-            <img src={logo} alt='logo' width='44px' />
-            <div className='header__title'>
-              Volley
-              <br /> Ball
-            </div>
+        <div className='header__logo'>
+          <img src={logo} alt='logo' width='44px' />
+          <div className='header__title'>
+            Volley
+            <br /> Ball
           </div>
-        </ul>
+        </div>
       </nav>
+      <Hamburger />
     </div>
   );
 };

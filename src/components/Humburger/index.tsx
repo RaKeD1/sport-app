6b7a993d6d00e799b72207d758a6c9fdb612e5f0
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2023-06-02 00:10:16 +0300

    delete trash

--- src/components/Humburger/index.tsx
+++ src/components/Humburger/index.tsx
@@ -29,27 +29,6 @@ const sidebar = {
     },
   },
 };
-// const sidebar2 = {
-//   open: (height = 1000) => ({
-//     clipPath: `circle(${height * 2 + 200}px at 100px 100px)`,
-//     visability: 'visible',
-//     transition: {
-//       type: 'spring',
-//       stiffness: 20,
-//       restDelta: 2,
-//     },
-//   }),
-//   closed: {
-//     clipPath: 'circle(40px at 435px 55px)',
-//     visability: 'hidden',
-//     transition: {
-//       delay: 0.3,
-//       type: 'spring',
-//       stiffness: 100,
-//       damping: 40,
-//     },
-//   },
-// };
 
 const Hamburger = ({ setIsOpen, isOpen }) => {
   const ref = useRef<HTMLDivElement>(null);

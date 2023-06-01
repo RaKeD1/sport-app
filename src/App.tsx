bb2b751f5592c4ce6978186d4046968904932884
KKuhta <124825110+KKuhta@users.noreply.github.com>
2023-06-01 17:11:37 +0300

    finished fix footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import './scss/app.scss';
-import Header from './components/Header';
-import Login from './pages/Login';
-import Registr from './pages/Registr';
-import Profile from './pages/Profile';
-import TrainingEdit from './pages/TrainingEdit';
-import React from 'react';
-import { RootState, useAppDispatch } from './redux/store';
-import { checkAuth } from './redux/slices/profileSlice';
-import { useSelector } from 'react-redux';
-import { Status } from './redux/slices/profileSlice';
-import CreateTrain from './components/CreateTrain';
-import Players from './pages/Players';
-import UpdateUser from './components/UpdateDataUser';
-import NotFound from './pages/NotFound/NotFound';
-import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
-import Statistics from './pages/Statistics';
-import Humburger from './components/Humburger';
-import Hamburger from './components/Humburger';
-import Footer from './components/Footer';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Registr from "./pages/Registr";
+import Profile from "./pages/Profile";
+import TrainingEdit from "./pages/TrainingEdit";
+import React from "react";
+import { RootState, useAppDispatch } from "./redux/store";
+import { checkAuth } from "./redux/slices/profileSlice";
+import { useSelector } from "react-redux";
+import { Status } from "./redux/slices/profileSlice";
+import CreateTrain from "./components/CreateTrain";
+import Players from "./pages/Players";
+import UpdateUser from "./components/UpdateDataUser";
+import NotFound from "./pages/NotFound/NotFound";
+import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
+import Statistics from "./pages/Statistics";
+import Humburger from "./components/Humburger";
+import Hamburger from "./components/Humburger";
+import Footer from "./components/Footer";
 
 function App() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const isAuth = useSelector((state: RootState) => state.profile.isAuth);
-  console.log('isAuth', isAuth);
+  console.log("isAuth", isAuth);
   const status = useSelector((state: RootState) => state.profile.status);
-  console.log('status', status);
+  console.log("status", status);
 
   React.useEffect(() => {
-    if (localStorage.getItem('token')) {
-      console.log(localStorage.getItem('token'));
+    if (localStorage.getItem("token")) {
+      console.log(localStorage.getItem("token"));
       dispatch(checkAuth());
     }
   }, []);
@@ -39,21 +39,22 @@ function App() {
     console.log(window.location.pathname);
     if (
       isAuth &&
-      (window.location.pathname === '/login' || window.location.pathname === '/registration')
+      (window.location.pathname === "/login" ||
+        window.location.pathname === "/registration")
     ) {
-      console.log('NAVIGATE /');
-      navigate('/');
+      console.log("NAVIGATE /");
+      navigate("/");
     } else if (
       !isAuth &&
       isAuth != null &&
-      window.location.pathname !== '/login' &&
-      window.location.pathname !== '/registration'
+      window.location.pathname !== "/login" &&
+      window.location.pathname !== "/registration"
     ) {
-      console.log('NAVIGATE /login');
-      navigate('/login');
+      console.log("NAVIGATE /login");
+      navigate("/login");
     } else if (isAuth === null) {
-      console.log('isAuth === null NAVIGATE /login');
-      navigate('/login');
+      console.log("isAuth === null NAVIGATE /login");
+      navigate("/login");
     }
   }, [isAuth]);
 
@@ -61,11 +62,11 @@ function App() {
 
   if (!isAuth) {
     return (
-      <div className='wrapper'>
-        <div className='container'>
+      <div className="wrapper">
+        <div className="container">
           <Routes>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/registration' element={<Registr />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/registration" element={<Registr />}></Route>
           </Routes>
         </div>
       </div>
@@ -74,18 +75,20 @@ function App() {
 
   return (
     <>
-      <Header />
-      <div className='container'>
-        <div className='content'>
-          <Routes>
-            <Route path='/' element={<Profile />}></Route>
-            <Route path='/profile' element={<Profile />}></Route>
-            <Route path='/createtraining' element={<CreateTrain />}></Route>
-            <Route path='/training' element={<TrainingEdit />}></Route>
-            <Route path='/statistics' element={<Statistics />}></Route>
-            <Route path='/players' element={<Players />}></Route>
-            <Route path='*' element={<NotFound />}></Route>
-          </Routes>
+      <div className='page'>
+        <Header />
+        <div className="container">
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Profile />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/createtraining" element={<CreateTrain />}></Route>
+              <Route path="/training" element={<TrainingEdit />}></Route>
+              <Route path="/statistics" element={<Statistics />}></Route>
+              <Route path="/players" element={<Players />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </div>
         </div>
       </div>
       <Footer />
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,79 +2,57 @@ import { FC, useEffect, useLayoutEffect, useState } from "react";
 import styles from "./footer.module.scss";
 
 const Footer: FC = () => {
-  const [hasScrollbar, setHasScrollbar] = useState(false);
-
-  useEffect(() => {
-    const handleScrollbar = () => {
-      const hasScrollbar = document.documentElement.scrollHeight > window.innerHeight;
-      setHasScrollbar(hasScrollbar);
-    };
-    window.addEventListener('resize', handleScrollbar);
-    handleScrollbar();
-    return () => {
-      window.removeEventListener('resize', handleScrollbar);
-    };
-  }, []);
-
-const containerStyle = {
-  position: hasScrollbar ? "relative" : "fixed",
-  width: "100%",
-  bottom: 0,
-} as React.CSSProperties;
-
   return (
-    <div style={containerStyle}>
-      <div className={styles.footer}>
-        <nav className="container">
-          <ul className={styles.footer__body}>
-            <div className={styles.footer__block}>
-              <h1 className={styles.footer__title}>
-                <a
-                  href="https://www.miet.ru/"
-                  target="MIET"
-                  className={styles.footer__title}
-                >
-                  МИЭТ
-                </a>
-              </h1>
+    <div className={styles.footer}>
+      <nav className="container">
+        <ul className={styles.footer__body}>
+          <div className={styles.footer__block}>
+            <h1 className={styles.footer__title}>
               <a
-                className={styles.footer__text}
-                href="https://yandex.ru/maps/org/moskovskiy_institut_elektronnoy_tekhniki/1042223652/?ll=37.208453%2C55.983384&z=17.08"
+                href="https://www.miet.ru/"
                 target="MIET"
+                className={styles.footer__title}
               >
-                площадь Шокина, 1
+                МИЭТ
               </a>
-            </div>
-            <div className={styles.footer__teacher}>
-              <h1 className={styles.footer__title}>Преподаватель</h1>
-              <p className={styles.footer__text}>Борисова Наталья Юрьевна</p>
-              <a
-                href="mailto:n.y.borisova@mail.ru"
-                className={styles.footer__text}
-              >
-                n.y.borisova@mail.ru
+            </h1>
+            <a
+              className={styles.footer__text}
+              href="https://yandex.ru/maps/org/moskovskiy_institut_elektronnoy_tekhniki/1042223652/?ll=37.208453%2C55.983384&z=17.08"
+              target="MIET"
+            >
+              площадь Шокина, 1
+            </a>
+          </div>
+          <div className={styles.footer__teacher}>
+            <h1 className={styles.footer__title}>Преподаватель</h1>
+            <p className={styles.footer__text}>Борисова Наталья Юрьевна</p>
+            <a
+              href="mailto:n.y.borisova@mail.ru"
+              className={styles.footer__text}
+            >
+              n.y.borisova@mail.ru
+            </a>
+          </div>
+          <div className={styles.footer__dev}>
+            <h1 className={styles.footer__title}>Разработано командой:</h1>
+            <p className={styles.footer__text}>
+              <a href="https://github.com/KKuhta" target="GitHub Kirill">
+                Кирилл
               </a>
-            </div>
-            <div className={styles.footer__dev}>
-              <h1 className={styles.footer__title}>Разработано командой:</h1>
-              <p className={styles.footer__text}>
-                <a href="https://github.com/KKuhta" target="GitHub Kirill">
-                  Кирилл
-                </a>
-                ,{" "}
-                <a href="https://github.com/RaKeD1" target="GitHub Danila">
-                  Данила
-                </a>
-                ,{" "}
-                <a href="https://github.com/Bogdan-67" target="GitHub Bogdan">
-                  Богдан
-                </a>
-              </p>
-            </div>
-          </ul>
-          {/* адрес миэта, ФИО препода, номер препода, почта препода, разработчики ФИО */}
-        </nav>
-      </div>
+              ,{" "}
+              <a href="https://github.com/RaKeD1" target="GitHub Danila">
+                Данила
+              </a>
+              ,{" "}
+              <a href="https://github.com/Bogdan-67" target="GitHub Bogdan">
+                Богдан
+              </a>
+            </p>
+          </div>
+        </ul>
+        {/* адрес миэта, ФИО препода, номер препода, почта препода, разработчики ФИО */}
+      </nav>
     </div>
   );
 };

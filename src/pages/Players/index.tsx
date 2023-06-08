6f25c23fc0d41ee729a15fa5a83a77ab9c3a5fa4
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2023-06-08 21:44:54 +0300

    add clearOnClick in input

--- src/pages/Players/index.tsx
+++ src/pages/Players/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useAppDispatch } from '../../redux/store';
 import {
   SelectInfoUsers,
@@ -200,20 +200,41 @@ export const Players = () => {
   const handlePageClick = (selected: number) => {
     setPage(selected);
   };
+  const inputRef = useRef<HTMLInputElement>(null);
   console.log('length', users.users.length);
-
+  const onClickClear = () => {
+    setValue('');
+    inputRef.current?.focus();
+  };
   return (
     <motion.div variants={pageMotion} initial='hidden' animate='show' exit='exit'>
       <div className={styles.main}>
         <div className={styles.form}>
           <label>Введите ФИО игрока:</label>
           <div className={styles.form__box}>
-            <input
-              className={styles.input}
-              type='text'
-              value={value}
-              onChange={(event) => setValue(event.target.value)}
-            />
+            <div className={styles.root}>
+              <input
+                ref={inputRef}
+                className={styles.input}
+                type='text'
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+              />
+              {value && (
+                <svg
+                  onClick={onClickClear}
+                  className={styles.iconClear}
+                  version='1.1'
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <g id='grid_system' />
+                  <g id='_icons'>
+                    <path d='M5.3,18.7C5.5,18.9,5.7,19,6,19s0.5-0.1,0.7-0.3l5.3-5.3l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3   c0.4-0.4,0.4-1,0-1.4L13.4,12l5.3-5.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L6.7,5.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4   l5.3,5.3l-5.3,5.3C4.9,17.7,4.9,18.3,5.3,18.7z' />
+                  </g>
+                </svg>
+              )}
+            </div>
+
             <div className={styles.form__roleButtons}>
               <button onClick={() => setGiveRolesModal(true)} className={styles.form__roleBtn}>
                 Выдать роль

015596395ce2feaa708081aa2e79133bb5ed5949
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2023-06-08 22:28:15 +0300

    fix modal UpdateDataUser

--- src/components/UpdateDataUser/index.tsx
+++ src/components/UpdateDataUser/index.tsx
@@ -134,265 +134,114 @@ const UpdateUser = (props) => {
       }
     }
   };
-
   return (
-    <div className={styles.update}>
-      <h2>Обновление данных пользователя</h2>
-      <div className={styles.update__blocks}>
-        <div className={(styles.update__leftBlock, styles.block)}>
-          <div>
-            <label>
-              Имя:
-              <input
-                type='text'
-                name='name'
-                value={formData.name}
-                onChange={handleChange}
-                autoComplete='off'
-                pattern='[^\d]{0,36}'
-                required
-              />
-              {errorsState.name && <div className={styles.error}>{errorsState.name}</div>}
-            </label>
+    formData.name && (
+      <div className={styles.update}>
+        <h2>Обновление данных пользователя</h2>
+        <div className={styles.update__blocks}>
+          <div className={(styles.update__leftBlock, styles.block)}>
+            <div>
+              <label>
+                Имя:
+                <input
+                  type='text'
+                  name='name'
+                  value={formData.name}
+                  onChange={handleChange}
+                  autoComplete='off'
+                  pattern='[^\d]{0,36}'
+                  required
+                />
+                {errorsState.name && <div className={styles.error}>{errorsState.name}</div>}
+              </label>
+            </div>
+            <div>
+              <label>
+                Фамилия:
+                <input
+                  type='text'
+                  name='surname'
+                  value={formData.surname}
+                  onChange={handleChange}
+                  pattern='[^\d]{0,36}'
+                  required
+                />
+                {errorsState.surname && <div className={styles.error}>{errorsState.surname}</div>}
+              </label>
+            </div>
+            <div>
+              <label>
+                Отчество (если имеется):
+                <input
+                  type='text'
+                  name='patronimyc'
+                  value={formData.patronimyc}
+                  onChange={handleChange}
+                  autoComplete='off'
+                />
+                {errorsState.patronimyc && (
+                  <div className={styles.error}>{errorsState.patronimyc}</div>
+                )}
+              </label>
+            </div>
           </div>
-          <div>
-            <label>
-              Фамилия:
-              <input
-                type='text'
-                name='surname'
-                value={formData.surname}
-                onChange={handleChange}
-                pattern='[^\d]{0,36}'
-                required
-              />
-              {errorsState.surname && <div className={styles.error}>{errorsState.surname}</div>}
-            </label>
-          </div>
-          <div>
-            <label>
-              Отчество (если имеется):
-              <input
-                type='text'
-                name='patronimyc'
-                value={formData.patronimyc}
-                onChange={handleChange}
-                autoComplete='off'
-              />
-              {errorsState.patronimyc && (
-                <div className={styles.error}>{errorsState.patronimyc}</div>
-              )}
-            </label>
+          <div className={(styles.update__rigthBlock, styles.block)}>
+            <div>
+              <label>
+                Почта:
+                <input
+                  type='email'
+                  name='email'
+                  value={formData.email}
+                  onChange={handleChange}
+                  autoComplete='off'
+                  maxLength={100}
+                  required
+                />
+                {errorsState.email && <div className={styles.error}>{errorsState.email}</div>}
+              </label>
+            </div>
+            <div>
+              <label className={styles.label}>
+                Телефон:
+                <MaskedInput
+                  required
+                  type='tel'
+                  name='phone'
+                  value={formData.phone}
+                  onChange={handleChange}
+                  autoComplete='off'
+                  mask={phoneNumberMask}
+                />
+                {errorsState.phone && <div className={styles.error}>{errorsState.phone}</div>}
+              </label>
+            </div>
+            <div>
+              <label>
+                Группа:
+                <input
+                  type='text'
+                  name='team'
+                  value={formData.team}
+                  onChange={handleChange}
+                  autoComplete='off'
+                  maxLength={50}
+                />
+                {errorsState.team && <div className={styles.error}>{errorsState.team}</div>}
+              </label>
+            </div>
           </div>
         </div>
-        <div className={(styles.update__rigthBlock, styles.block)}>
-          <div>
-            <label>
-              Почта:
-              <input
-                type='email'
-                name='email'
-                value={formData.email}
-                onChange={handleChange}
-                autoComplete='off'
-                maxLength={100}
-                required
-              />
-              {errorsState.email && <div className={styles.error}>{errorsState.email}</div>}
-            </label>
-          </div>
-          <div>
-            <label className={styles.label}>
-              Телефон:
-              <MaskedInput
-                required
-                type='tel'
-                name='phone'
-                value={formData.phone}
-                onChange={handleChange}
-                autoComplete='off'
-                mask={phoneNumberMask}
-              />
-              {errorsState.phone && <div className={styles.error}>{errorsState.phone}</div>}
-            </label>
-          </div>
-          <div>
-            <label>
-              Группа:
-              <input
-                type='text'
-                name='team'
-                value={formData.team}
-                onChange={handleChange}
-                autoComplete='off'
-                maxLength={50}
-              />
-              {errorsState.team && <div className={styles.error}>{errorsState.team}</div>}
-            </label>
-          </div>
-        </div>
+        <button
+          type='submit'
+          style={{ opacity: disable ? '0.5' : '1' }}
+          onClick={handleUpdateUser}
+          disabled={disable}>
+          Обновить
+        </button>
       </div>
-      <button
-        type='submit'
-        style={{ opacity: disable ? '0.5' : '1' }}
-        onClick={handleUpdateUser}
-        disabled={disable}>
-        Обновить
-      </button>
-    </div>
+    )
   );
 };
 
 export default UpdateUser;
-
-// import { FC, useState, useEffect } from 'react';
-// import { useAppDispatch, useAppSelector } from '../../hooks/redux';
-// import { setUpdateUserStatus, updateUser } from '../../redux/slices/profileSlice';
-// import { useSelector } from 'react-redux';
-// import styles from './UpdateDataUser.module.scss';
-// import MaskedInput from 'react-text-mask';
-// import { phoneNumberMask } from '../RegistrForm';
-
-// const UpdateUser = (props) => {
-//   console.log('props', props);
-//   const dispatch = useAppDispatch();
-//   const updateUserStatus = useSelector(setUpdateUserStatus);
-//   const [nameState, setNameState] = useState<string>('');
-//   const [surnameState, setSurnameState] = useState<string>('');
-//   const [patronimycState, setPatronimycState] = useState<string>('');
-//   const [emailState, setEmailState] = useState<string>('');
-//   const [phoneState, setPhoneState] = useState<string>('');
-//   const [disable, setDisable] = useState<boolean>(true);
-
-//   const [errors, setErrors] = useState({
-//     name: 'Проверьте правильно введенных данных',
-//     surname: '',
-//     error:false,
-//     // Add more fields if needed
-//   });
-
-//   useEffect(() => {
-//     setNameState(props.user.name);
-//     setSurnameState(props.user.surname);
-//     setPatronimycState(props.user.patronimyc);
-//     setEmailState(props.user.email);
-//     setPhoneState(props.user.phone);
-//     setDisable(true);
-//   }, [props.user]);
-
-//   useEffect(() => {
-//     console.log('произошло изменение ', disable);
-//     if (
-//       (nameState !== props.user.name ||
-//         surnameState !== props.user.surname ||
-//         patronimycState !== props.user.patronimyc ||
-//         emailState !== props.user.email ||
-//         phoneState !== props.user.phone) &&
-//       errors.error
-//     ) {
-//       setDisable(false);
-//     }
-//   }, [nameState]);
-
-//   const handleUpdateUser = () => {
-//     if (window.confirm('Обновить данные?')) {
-//       const updatedUserData = {
-//         name: nameState,
-//         surname: surnameState,
-//         patronimyc: patronimycState,
-//         email: emailState,
-//         phone: phoneState,
-//         login: props.user.login,
-//       };
-//       props.setIsActive(false);
-//       dispatch(updateUser({ id_user: props.user.id_user, userData: updatedUserData }));
-//       if (props.isUpdate) {
-//         props.isUpdate(true);
-//       }
-//     }
-//   };
-
-//   return (
-//     <div className={styles.update}>
-//       <h2>Обновление данных пользователя</h2>
-//       <div className={styles.update__blocks}>
-//         <div className={(styles.update__leftBlock, styles.block)}>
-//           <div>
-//             <label>
-//               Имя:
-//               <input
-//                 type='text'
-//                 value={nameState}
-//                 onChange={handleNameChange}
-//                 autoComplete='off'
-//                 pattern='[^\d]{0,36}'
-//                 required
-//               />
-//               {nameError && <div>{nameError}</div>}
-//             </label>
-//           </div>
-//           <div>
-//             <label>
-//               Фамилия:
-//               <input
-//                 type='text'
-//                 value={surnameState}
-//                 onChange={(e) => setSurnameState(e.target.value)}
-//                 pattern='[^\d]{0,36}'
-//                 required
-//               />
-//             </label>
-//           </div>
-//           <div>
-//             <label>
-//               Отчество (если имеется):
-//               <input
-//                 type='text'
-//                 value={patronimycState}
-//                 onChange={(e) => setPatronimycState(e.target.value)}
-//                 autoComplete='off'
-//               />
-//             </label>
-//           </div>
-//         </div>
-//         <div className={(styles.update__rigthBlock, styles.block)}>
-//           <div>
-//             <label>
-//               Почта:
-//               <input
-//                 type='email'
-//                 value={emailState}
-//                 onChange={(e) => setEmailState(e.target.value)}
-//                 autoComplete='off'
-//                 required
-//               />
-//             </label>
-//           </div>
-//           <div>
-//             <label className={styles.label}>
-//               Телефон:
-//               <MaskedInput
-//                 required
-//                 type='tel'
-//                 name='phone'
-//                 value={phoneState}
-//                 onChange={(e) => setPhoneState(e.target.value)}
-//                 autoComplete='off'
-//                 mask={phoneNumberMask}
-//               />
-//             </label>
-//           </div>
-//         </div>
-//       </div>
-//       <button
-//         type='submit'
-//         style={{ opacity: disable ? '0.5' : '1' }}
-//         onClick={handleUpdateUser}
-//         disabled={disable}>
-//         Обновить
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default UpdateUser;
